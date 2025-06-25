0a4ef97cedfc07a0446684a7331016e5f57ff87a
Reece Hart <rhart@bridgeanalytics.org>
2025-06-25 12:33:58 -0700

    add support for loading data from cli

--- src/ppmidb/cli.py
+++ src/ppmidb/cli.py
@@ -2,6 +2,7 @@
 
 """infer schema from provided csv file"""
 
+import csv
 from io import StringIO
 import logging
 from pathlib import Path
@@ -41,10 +42,6 @@ def cli(verbose, config):
     import coloredlogs
     coloredlogs.install(level="INFO")
 
-    click.echo(f"Global Verbose mode: {'ON' if verbose else 'OFF'}")
-    if config:
-        click.echo(f"Using global config: {config}")
-
     # You could store config/verbose in ctx.obj if desired for more complex shared state
     # click.get_current_context().obj = {'verbose': verbose, 'config': config}
 
@@ -52,8 +49,12 @@ def cli(verbose, config):
 @cli.command("generate-schema")
 @click.argument("csv_path", type=click.Path(exists=True, dir_okay=False, readable=True))
 def generate_schema(csv_path: str):
+    csv_content = read_csv_content(csv_path)
+    table_name = Path(csv_path).stem
+    table_name = clean_for_sql_name(table_name)
+    table_name = re.sub(r"_\d{8}$", r"", table_name)
+
     try:
-        csv_content = read_csv_content(csv_path)
         df = pl.read_csv(
             StringIO(csv_content),
             has_header=True,
@@ -62,10 +63,6 @@ def generate_schema(csv_path: str):
         )
     except Exception as e:
         raise RuntimeError(f"Error reading CSV file '{csv_path}': {e}")
-
-    table_name = Path(csv_path).stem
-    table_name = clean_for_sql_name(table_name)
-    table_name = re.sub(r"_\d{8}$", r"", table_name)
 
     schema = infer_schema(df)
     table = schema_as_table(schema)
@@ -84,18 +81,39 @@ def generate_schema(csv_path: str):
 @cli.command("generate-copy")
 @click.argument("csv_path", type=click.Path(exists=True, dir_okay=False, readable=True))
 def generate_copy(csv_path: str):
-    try:
-        csv_content = read_csv_content(csv_path)
-    except Exception as e:
-        raise RuntimeError(f"Error reading CSV file '{csv_path}': {e}")
-
+    csv_content = read_csv_content(csv_path)
     table_name = Path(csv_path).stem
     table_name = clean_for_sql_name(table_name)
     table_name = re.sub(r"_\d{8}$", r"", table_name)
 
     print(f"COPY {table_name} from STDIN with (format csv, header true);")
     print(csv_content)
-    print("\.")
+    print("\\.")
+
+
+@cli.command("load")
+@click.argument("csv_path", type=click.Path(exists=True, dir_okay=False, readable=True))
+@click.option("--uri", type=str)
+def load(csv_path: str, uri: str):
+    import psycopg
+
+    table_name = Path(csv_path).stem
+    table_name = clean_for_sql_name(table_name)
+    table_name = re.sub(r"_\d{8}$", r"", table_name)
+
+    csv_content = read_csv_content(csv_path)
+
+    con = psycopg.connect(uri)
+    cur = con.cursor()
+
+    csv_rdr = csv.reader(StringIO(csv_content))
+    header = next(csv_rdr)
+    query = f"COPY {table_name} ({','.join(header)}) from STDIN"
+    query += " WITH (FORMAT CSV, HEADER)"
+
+    with cur.copy(query) as copy:
+        copy.write(csv_content)
+    con.commit()
 
 
 if __name__ == "__main__":
